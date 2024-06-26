a800feb194eff47f7b01bb46bddc0d025a841d57
Uigor Teodoro <107517025+Uigorteodoro@users.noreply.github.com>
2024-06-25 21:55:44 -0300

    alterando a conexao com o banco

--- mac6/config/Database.php
+++ mac6/config/Database.php
@@ -1,16 +1,16 @@
 <?php
-
 class Database {
-    private $host = "localhost";
+    private $host = "127.0.0.1";
+    private $port = "3308"; // Porta padrão do MySQL/MariaDB
     private $user = "root";
-    private $db = "tarefa";
-    private $pwd = "123456789";
+    private $db = "mydb";
+    private $pwd = "123";
     private $conn = NULL;
 
     public function connect() {
 
         try{
-            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pwd);
+            $this->conn = new PDO("mysql:host=$this->host;port=$this->port;dbname=$this->db", $this->user, $this->pwd);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             echo "Connection Error: " . $e->getMessage();
